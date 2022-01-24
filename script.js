61884c95cb9f4a8bd51c4a2766657960ae0bb731
Flavius Petruca <fpetruca@albelli.net>
2022-01-24 13:47:10 +0100

    Fix string instead of number and will remove previous output

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//printResults will check if the number that was inputed is valid and will compute the result based on the number
+
 const printResults = () => {
     const outPutText = document.getElementById("outputText");
     const number = document.getElementById("input").value;
@@ -5,38 +7,45 @@ const printResults = () => {
     
     let divisible = new Divisible(outPutText, number, submitBtn);
 
-    divisible.validate();
+    if (!divisible.validate()){
+        return;
+    };
+    divisible.emptyOutput();
     divisible.computeResult();
 
     setError("");
 }
 
-const setError = (err) => {
-    const error = document.getElementById("error");
-    error.textContent = err;
-}
 
+//to move to another file
 class Divisible {
     constructor(outPutText, number, submitBtn){
         this.outPutText = outPutText;
-        this.number = number;
+        this.number = parseFloat(number);
         this.submitBtn = submitBtn;
     }
 
     validate(){
         if (this.number === "" || isNaN(this.number)){
-            setError("You didn't add a this.number in!");
-            return;
+            setError("You didn't add a number in!");
+            return false;
         }
-        
+
         if (!Number.isInteger(this.number)){
             setError("Number must be natural!")
-            return;
+            return false;
         }
     
         if (this.number <= 0 && this.number > 10000){
             setError("Number must be higher then 0 and smaller than 10000");
-            return;
+            return false;
+        }
+        return true;
+    }
+
+    emptyOutput(){
+        while (this.outPutText.firstChild){
+            this.outPutText.removeChild(this.outPutText.lastChild);
         }
     }
 
@@ -56,4 +65,9 @@ class Divisible {
         }
         this.outPutText.append(text);
     }
+}
+
+const setError = (err) => {
+    const error = document.getElementById("error");
+    error.textContent = err;
 }
