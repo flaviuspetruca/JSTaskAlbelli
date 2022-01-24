609da2dff7c02d7e11de166814563e669426b777
Flavius Petruca <fpetruca@albelli.net>
2022-01-24 12:11:13 +0100

    Change to class

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
 const printResults = () => {
+    const outPutText = document.getElementById("outputText");
+    const number = document.getElementById("input").value;
+    const submitBtn = document.getElementById("submitBtn");
+    
+    let divisible = new Divisible(outPutText, number, submitBtn);
 
-    const outPutText    = document.getElementById("outputText");
-    const number        = document.getElementById("input").value;
-    const submitBtn     = document.getElementById("submitBtn");
+    divisible.validate();
+    divisible.computeResult();
 
-    if(number == "" || isNaN(number)){
-        setError("You didn't add a number in!");
-        return;
-    }
-
-    if(number <= 0){
-        setError("Number must be higher then 0");
-        return;
-    }
-
-    if(submitBtn.value){
-        while (outPutText.firstChild){
-            outPutText.removeChild(outPutText.lastChild);
-        }
-    }
-
-    for(let i = 1 ; i <= number ; i++){
-        const text = document.createElement("h3");
-        if(i % 2 == 0 && i % 3 == 0){
-            text.append("foo bar");
-        } else if(i % 2 == 0){
-            text.append("foo");
-        } else if(i % 3 == 0){
-            text.append("bar");
-        } else {
-            text.append(i);
-        }
-        outPutText.append(text);
-    }
-
-    submitBtn.setAttribute("value", 1);
     setError("");
 }
 
 const setError = (err) => {
-    const error         = document.getElementById("error");
-    error.textContent   = err;
+    const error = document.getElementById("error");
+    error.textContent = err;
+}
+
+class Divisible {
+    constructor(outPutText, number, submitBtn){
+        this.outPutText = outPutText;
+        this.number = number;
+        this.submitBtn = submitBtn;
+    }
+
+    validate(){
+        if (this.number === "" || isNaN(this.number)){
+            setError("You didn't add a this.number in!");
+            return;
+        }
+        
+        if (!Number.isInteger(this.number)){
+            setError("Number must be natural!")
+            return;
+        }
+    
+        if (this.number <= 0 && this.number > 10000){
+            setError("Number must be higher then 0 and smaller than 10000");
+            return;
+        }
+    }
+
+    computeResult(){
+        const text = []
+        let i = 1 ;
+        while (i <= this.number){
+            if (i % 2 === 0 && i % 3 === 0){
+                text.push("foo bar");
+            } else if (i % 2 === 0){
+                text.push("foo");
+            } else if (i % 3 === 0){
+                text.push("bar");
+            } else {
+                text.push(i);
+            }
+            i++;
+        }
+        this.outPutText.append(text);
+    }
 }
